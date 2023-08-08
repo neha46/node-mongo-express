f27d45f89103152abc33a65662521050f1c8d2da
Neha <nehamishra6714@gmail.com>
2023-08-08 18:34:50 +0530

    node js  learen

---
+++ index.js
@@ -0,0 +1,28 @@
+//to create http server--------->
+// const http=require('http')
+
+// function data(req,res){
+
+//     res.write("<h1>hiiiii</h1>")
+//     res.end();
+// }
+// const server=http.createServer(data)
+
+// server.listen(2000);
+// //ch1
+// console.log("hello ansiha mishra");
+
+//ch2
+// (function(exports,require,module,__dirname,__filename)
+// {
+
+// });
+// console.log("hi  Neha")
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(module);
+
+//ch3
+const  path=require('path');
+
+
